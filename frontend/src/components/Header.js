c31e3ca6817d6061adcecd69ac56841076ec76b7
Tracie <traciecodes@gmail.com>
2021-03-03 19:24:58 -0600

    Adds react router and creates separate screens for individual products

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
@@ -6,16 +7,23 @@ const Header = () => {
     <header>
       <Container>
         <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-          <Navbar.Brand href='/'>Proshop Tech</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Proshop Tech</Navbar.Brand>
+          </LinkContainer>
+
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link href='/cart'>
-                <i class='fas fa-shopping-cart'></i>Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <i class='fas fa-sign-in-alt'></i>Log In
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i class='fas fa-shopping-cart'></i>Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i class='fas fa-sign-in-alt'></i>Log In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
