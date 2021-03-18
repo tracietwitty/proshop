2f85e0094f6f36314b287da895388d71b7877f50
Tracie <traciecodes@gmail.com>
2021-03-18 13:30:45 -0500

    Prepares data to import directly into database instead of from .js file and creates a seeder for dummy data

---
+++ backend/seeder.js
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import users from './data/users.js';
+import products from './data/products.js';
+import User from './models/userModel.js';
+import Product from './models/productModel.js';
+import Order from './models/orderModel.js';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+connectDB();
+
+const importData = async () => {
+  try {
+    // completely wipes any existing info in the db to start with a clean slate:
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    // imports desired user info:
+    const createdUsers = await User.insertMany(users);
+
+    const adminUser = createdUsers[0]._id;
+
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser };
+    });
+
+    await Product.insertMany(sampleProducts);
+
+    console.log('Data imported'.green);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+  }
+};
+
+const destroyData = async () => {
+  try {
+    // completely wipes any existing info in the db to start with a clean slate:
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    console.log('Data destroyed'.green);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+  }
+};
+
+if (process.argv(2) === '-d') {
+  destroyData();
+} else {
+  importData();
+}
