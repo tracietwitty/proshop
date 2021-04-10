dd28cb562ddfb8791b033015651102a306f023fa
Tracie <traciecodes@gmail.com>
2021-04-09 19:36:15 -0500

    Sets up web tokens for auth users and starts adding profile to routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import User from '../models/userModel.js';
 import asyncHandler from 'express-async-handler';
+import generateToken from '../utils/generateToken.js';
 
 // @desc    Authenticates user & gets token:
 // @route   POST /api/users/login
@@ -15,7 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
@@ -23,4 +24,11 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+// @desc    Gets user profile
+// @route   GET /api/users/profile
+// @access  Public route
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.send('success');
+});
+
+export { authUser, getUserProfile };
