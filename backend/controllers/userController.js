f22896a457c1db4af51ec5674e8b3e32a412e0d6
Tracie <traciecodes@gmail.com>
2021-04-12 14:22:29 -0500

    Creates authorization middleware for protected routes in user login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,9 +26,21 @@ const authUser = asyncHandler(async (req, res) => {
 
 // @desc    Gets user profile
 // @route   GET /api/users/profile
-// @access  Public route
+// @access  Private route
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('success');
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 export { authUser, getUserProfile };
