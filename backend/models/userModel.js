c66646bb2a0998c7215e7691976d1e0426f72c52
Tracie <traciecodes@gmail.com>
2021-04-12 14:44:17 -0500

    Creates backend functionality for registering new users

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -33,6 +33,14 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 // 2. creates a model from the schema above:
 const User = mongoose.model('User', userSchema);
 
